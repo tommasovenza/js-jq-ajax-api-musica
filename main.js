c02dc3787175bf2687ebe5e8e17d282f4f664cbe
Tommaso Venza <tommasovenza@gmail.com>
2020-06-26 15:50:45 +0200

    setup files

---
+++ main.js
@@ -0,0 +1,11 @@
+// Attraverso una chiamata ajax all’Api di boolean avremo a
+// disposizione una decina di dischi musicali.
+// Servendoci di handlebars stampiamo tutto a schermo.
+// In questo momento non è importante la parte grafica.
+// Bonus: Creare una select con i seguenti generi: pop, rock,
+// metal e jazz. In base a cosa scegliamo nella select vedremo i
+// corrispondenti cd.
+
+$(document).ready(function() {
+	//Code
+});
