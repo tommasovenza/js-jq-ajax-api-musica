a8265db259c56441b11951f3e450e795314063af
Tommaso Venza <tommasovenza@gmail.com>
2020-06-26 18:55:29 +0200

    esercizio svolto

--- main.js
+++ main.js
@@ -6,6 +6,45 @@
 // metal e jazz. In base a cosa scegliamo nella select vedremo i
 // corrispondenti cd.
 
-$(document).ready(function() {
-	//Code
-});
+$(document).ready(function () {
+
+	// chiamata ajax all'api di boolean per avere i 10 dischi
+	$.ajax({
+
+		url: "https://flynn.boolean.careers/exercises/api/array/music",
+		method: 'GET',
+
+		success: function (data) {
+
+			var dischi = data.response;
+
+			generaDischi(dischi);
+
+		},
+
+		error: function () {
+			alert('quaclosa non va');
+		}
+		
+	}); // end ajax
+
+
+	function generaDischi(arrayDischi) {
+
+		for (var i = 0; i < arrayDischi.length; i++) {
+
+			var singoloDisco = arrayDischi[i];
+
+			// handlebars
+			var source = $('#disco-template').html();
+			var template = Handlebars.compile(source);
+
+			// genera template con i parametri passati
+			var htmlGenerato = template(singoloDisco);
+
+			$('.cds-container').append(htmlGenerato);
+		}
+	}
+
+
+}); // end document ready
